26b7af9a2cc2663b5cf47faaeeeee6dd394972cf
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2025-08-03 07:19:20 +0200

    Add remote answers

--- bin/quiz_check.js
+++ bin/quiz_check.js
@@ -2,57 +2,68 @@
 
 const fs = require('fs');
 const c = require('ansi-colors');
+require('dotenv').config();
 
-const answerFile = './.answers';
+const ANSWERS = process.env.ANSWERS;
 
-const answers = Object.fromEntries(
-                    fs.readFileSync(answerFile,'utf-8').split("\n").filter( x => x.trim() !== "").map(x => x.split(/\s+/g))
-                );
+main();
 
+async function main() {
+    const answerData = await fetchFile(ANSWERS);
 
-const report = process.argv[2];
+    const answers = Object.fromEntries(
+                        answerData.split("\n").filter( x => x.trim() !== "").map(x => x.split(/\s+/g))
+                    );
 
-if (! report) {
-    console.error("usage: quiz_check.js report");
-    process.exit(1);
+    const report = process.argv[2];
+
+    if (! report) {
+        console.error("usage: quiz_check.js report");
+        process.exit(1);
+    }
+
+    const reportLines = fs.readFileSync(report,'utf-8').split("\n");
+
+    let quiz;
+    let read = false;
+    let answer = '';
+    let score = 0;
+    reportLines.forEach(line => {
+        if (line.match(/^=== (\w+)/)) {
+            quiz = line.replace(/^=== (\S+) .*/g,"$1");
+            read = true;
+            return;
+        }
+        if (read) {
+            if (line.match(/.*CONFLICT/)) {
+            answer = 'Conflict';
+            }
+            else if (line.match(/No conflict/)) {
+            answer = 'NonConflict';
+            }
+            else {
+            answer = 'Unknown';
+            }
+            let ok = false;
+            if (answers[quiz] === answer) {
+            ok = true;
+            score++;
+            }
+
+            if (ok) {
+                console.log(`${quiz} expect ${answers[quiz]} got ${answer} -> ` + c.green(ok));
+            }
+            else {
+                console.log(c.cyan(quiz) + ` expect ` + c.green(answers[quiz]) + ` got ` + c.yellow(answer) + ` -> ` + c.red(ok));
+            }
+            read = false;
+        }
+    });
+
+    console.log("\nScore: " + c.bold(100*score/Object.keys(answers).length) + "%");
 }
 
-const reportLines = fs.readFileSync(report,'utf-8').split("\n");
-
-let quiz;
-let read = false;
-let answer = '';
-let score = 0;
-reportLines.forEach(line => {
-    if (line.match(/^=== (\w+)/)) {
-        quiz = line.replace(/^=== (\S+) .*/g,"$1");
-        read = true;
-        return;
-    }
-    if (read) {
-        if (line.match(/.*CONFLICT/)) {
-           answer = 'Conflict';
-        }
-        else if (line.match(/No conflict/)) {
-           answer = 'NonConflict';
-        }
-        else {
-           answer = 'Unknown';
-        }
-        let ok = false;
-        if (answers[quiz] === answer) {
-           ok = true;
-           score++;
-        }
-
-        if (ok) {
-            console.log(`${quiz} expect ${answers[quiz]} got ${answer} -> ` + c.green(ok));
-        }
-        else {
-            console.log(c.cyan(quiz) + ` expect ` + c.green(answers[quiz]) + ` got ` + c.yellow(answer) + ` -> ` + c.red(ok));
-        }
-        read = false;
-    }
-});
-
-console.log("\nScore: " + c.bold(100*score/Object.keys(answers).length) + "%");
+async function fetchFile(url) {
+    const respose = await fetch(url);
+    return await respose.text();
+}
